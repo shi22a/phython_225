29f948ed0dd85a160035cbebc512f157c5ce499a
shiva <shi22a@yahoo.com>
2019-01-09 19:08:10 +0330

    game:make story with another person's idea

---
+++ 02.py
@@ -0,0 +1,11 @@
+
+#story make
+print ("shiva is starting")
+story="emroz sobh vaghti ke %s " \
+      "az khab bidar shod hese %s dasht " \
+      "chon o %s sale shode bod. "
+name=input("enter a name: ")
+adj=input("enter a adjective: ")
+num=input("enter a number: ")
+print(story %(name,adj,num))
+
