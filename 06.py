7491e324e9a47d2ce856987583b9b4c8f2fdd4ed
shiva <shi22a@yahoo.com>
2019-01-12 19:47:04 +0330

    def

--- 06.py
+++ 06.py
@@ -1,8 +1,11 @@
 
 # factorial
 num=int(input("Enter a number: "))
-f=1
-for i in range(1,num+1):
-    f *=i
 
-print("factorial (%s) = %s" % (num,f))
+def factorial(n):
+   f=1
+   for i in range(1,n+1):
+    f *=i
+   return f
+
+print("factorial (%s) = %s" % (num,factorial(num)))
