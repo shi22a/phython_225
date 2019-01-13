af01a4dbc05876359a9b4b5b5aa6e8ad1da7906d
shiva <shi22a@yahoo.com>
2019-01-13 10:20:48 +0330

    up-letter

--- 07.py
+++ 07.py
@@ -1,7 +1,8 @@
+#Write a program that accepts sequence of lines as input
+# and prints the lines after making all characters in the sentence capitalized.
 
 
-
-s="Hello Word"
+s=input("Enter a sentence: ")
 
 def up_letter(x):
     l=[]
