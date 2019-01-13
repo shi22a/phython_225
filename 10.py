4b304ba8992398f9f43d47eece8ad6d98eacb254
shiva <shi22a@yahoo.com>
2019-01-13 14:18:03 +0330

    second highest grade

---
+++ 10.py
@@ -0,0 +1,12 @@
+
+
+n=4
+stu=["a","b","c","d"]
+num=[10,15,9,20]
+mark=[]
+for i in range(4):
+    mark.append([stu[i],num[i]])
+print(mark)
+print(sorted(set(list(marks for name,marks in mark)))[1])
+
+
